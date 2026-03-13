8befff68bc070ee1cf5ead8885194f9d8fb22902
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-13 20:31:21 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string mes;
+Console.WriteLine("Введите название предмета: ф, м, и, г, б");
+string subject = Console.ReadLine();
+switch (subject)
+{case "ф": mes = "Физика"; break;
+ case "м": mes = "Математика"; break;
+ case "и": mes = "История"; break;
+ case "г": mes = "География"; break;
+ case "б": mes = "Биология"; break;
+ default: mes = "Некорректная буква предмета"; break;
+}
+Console.WriteLine(mes);
+Console.ReadKey();
